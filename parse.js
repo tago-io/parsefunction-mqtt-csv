97d8b6be8c233f8c861602f68191131e51a6016a
Fabio Rosa <fabioer3030@gmail.com>
2018-12-05 18:50:25 -0500

    change comments

--- parse.js
+++ parse.js
@@ -1,16 +1,18 @@
 /**
- * The payload variable is an array and it's global
- * You can add items, remove and edit
+ * This code finds the variable temperature inside the payload posted by the device,
+ * converts the value from Fahrenheit to Celsius, 
+ * and changes its value to Celsius, and adds the unit
  */
 
-// First, we find by temperature variable on payload(array)
+// First, we find the temperature variable inside the payload(array)
 const temperatureItem = payload.find(item => item.variable === 'temperature');
 
-// If temperature variable we change the actual value from Fahrenheit to Celsius
+// If we find temperature variable, we convert the value from Fahrenheit to Celsius
 if (temperatureItem) {
   const actualTemperatureInFahrenheit = temperatureItem.value;
   const celsius = (5 / 9) * (actualTemperatureInFahrenheit - 32);
 
+// Then, we output the value and unit.
   temperatureItem.value = celsius;
   temperatureItem.unit = 'C';
 }
